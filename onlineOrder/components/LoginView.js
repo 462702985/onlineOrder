0a76faded08b7be2808566f39cdb40fa44e2d804
YONG LI <85102180+462702985@users.noreply.github.com>
2022-01-03 15:42:51 -0500

    Add files via upload

---
+++ onlineOrder/components/LoginView.js
@@ -0,0 +1,14 @@
+function Loginview() {
+    return (
+        <div style={
+            {display:"flex",
+            justifyContent: "space-evenly"}}>
+            <img src="mcd.png" alt="mcd"></img>
+            <img src="bgk.png" alt="bgk"></img>
+            <img src="joker.jpg" alt="joker"></img>
+        </div>
+        
+    )
+}
+
+export default Loginview;
